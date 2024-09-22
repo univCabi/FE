09f63fd7d8ce392fa4b21d123ce78add44f55b14
yemin4242 <whereismysejong>
2024-09-22 16:34:16 +0900

    Style: Change Button CSS in MainPage

--- univ_fe/src/pages/main.tsx
+++ univ_fe/src/pages/main.tsx
@@ -2,7 +2,7 @@ const MainPage = () => {
   return (
     <div>
       <h1>main</h1>
-      <button>go to home page</button>
+      <button className="font-bold">go to home page</button>
     </div>
   );
 };
