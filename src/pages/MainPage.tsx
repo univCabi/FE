899d36b30db6b288cec56191dc947244bec33f48
KimJuheee <3098k@naver.com>
2024-10-26 17:49:23 +0900

    Fix: 사물함 겹치는 오류 해결#14

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -26,7 +26,7 @@ const MainPage = () => {
 
   return (
     <div>
-      {/* 상단 네비게이션바 */}
+      {/* 상단 네비게이션바(화면 크기 상관없이 표시) */}
       <SideNavigationLayout
         buildings={buildings}
         selectedBuilding={selectedBuilding}
@@ -36,46 +36,10 @@ const MainPage = () => {
         setIsOpen={setIsOpen}
       />
 
-      {/* 건물 정보(좌측) */}
-      <div className="absolute inset-y-0 left-0 w-40 border-r-2 border-gray-400 flex-col pt-20 hidden md:flex">
-        <BuildingSelectButton
-          buildings={buildings}
-          selectedBuilding={selectedBuilding}
-          setSelectedBuilding={setSelectedBuilding}
-          selectedFloor={selectedFloor}
-          setSelectedFloor={setSelectedFloor}
-        />
-
-        {/* 하단 메뉴(좌측) */}
-        <CabinetFooterMenuButton />
-      </div>
-
-      {/* 사물함 위치(중앙) */}
-      <div className="absolute inset-y-0 left-0 right-0 md:left-64 md:right-80 border-gray-400 pt-20 flex">
-        {/* 건물 선택 후, 층수 선택을 둘 다 해야 사물함 컴포넌트가 보임 */}
-        {selectedBuilding !== null && selectedFloor !== null && (
-          <>
-            <CabinetButtonComponent
-              rows={4}
-              columns={12}
-              selectedBuilding={buildings[selectedBuilding]}
-              selectedFloor={buildings[selectedBuilding]?.floors[selectedFloor]}
-              setSelectedCabinet={setSelectedCabinet}
-            />
-            <CabinetStatusInformation />
-          </>
-        )}
-      </div>
-
-      {/* 선택한 사물함 정보(우측) */}
-      <div className="absolute inset-y-0 right-0 w-80 border-gray-400 border-l-2 pt-20 hidden md:flex">
-        <SelectedCabinetInformation selectedCabinet={selectedCabinet} />
-      </div>
-
-      {/* 아래는 화면 축소(md)일 때의 코드 */}
-      {/* 건물 & 층 선택 안했을 때는 건물 & 층 선택하는 컴포넌트만 표시 */}
-      {selectedFloor === null && (
-        <div className="absolute inset-y-0 left-0 w-40 border-r-2 border-gray-400 flex-col pt-20 flex md:hidden">
+      {/* 화면 크기 = 768px 이상일 때 */}
+      <div className="md:flex">
+        {/* 건물 정보(좌측) */}
+        <div className="absolute inset-y-0 left-0 w-40 border-r-2 border-gray-400 flex-col pt-20 hidden md:flex">
           <BuildingSelectButton
             buildings={buildings}
             selectedBuilding={selectedBuilding}
@@ -83,16 +47,86 @@ const MainPage = () => {
             selectedFloor={selectedFloor}
             setSelectedFloor={setSelectedFloor}
           />
+
           {/* 하단 메뉴(좌측) */}
           <CabinetFooterMenuButton />
         </div>
-      )}
-      {/* 사물함 선택 -> cabinetRental 컴포넌트 표시 */}
-      {selectedCabinet && (
-        <div className="absolute inset-y-0 right-0 w-80 border-gray-400 border-l-2 pt-20 flex md:hidden">
+
+        {/* 사물함 위치(중앙) */}
+        <div className="absolute inset-y-0 left-0 right-0 md:left-64 md:right-80 border-gray-400 pt-16 hidden md:flex">
+          {/* 건물 선택 후, 층수 선택을 둘 다 해야 사물함 컴포넌트가 보임 */}
+          {selectedBuilding !== null && selectedFloor !== null && (
+            <>
+              <CabinetButtonComponent
+                rows={4}
+                columns={12}
+                selectedBuilding={buildings[selectedBuilding]}
+                selectedFloor={
+                  buildings[selectedBuilding]?.floors[selectedFloor]
+                }
+                setSelectedCabinet={setSelectedCabinet}
+              />
+              <CabinetStatusInformation />
+            </>
+          )}
+        </div>
+        {/* 선택한 사물함 정보(우측) */}
+        <div className="absolute inset-y-0 right-0 w-80 border-gray-400 border-l-2 pt-20 hidden md:flex">
           <SelectedCabinetInformation selectedCabinet={selectedCabinet} />
         </div>
-      )}
+      </div>
+
+      {/* 화면 크기 = 768px 이하일 때 */}
+      <div className="md:hidden">
+        {/* 건물 & 층 선택 안했을 때 -> 건물 & 층 선택하는 컴포넌트만 표시 */}
+        {selectedFloor === null && (
+          <div className="absolute inset-y-0 left-0 w-40 border-r-2 border-gray-400 flex-col pt-20 flex">
+            <BuildingSelectButton
+              buildings={buildings}
+              selectedBuilding={selectedBuilding}
+              setSelectedBuilding={setSelectedBuilding}
+              selectedFloor={selectedFloor}
+              setSelectedFloor={setSelectedFloor}
+            />
+            {/* 하단 메뉴(좌측) */}
+            <CabinetFooterMenuButton />
+          </div>
+        )}
+
+        {/* 건물&층 선택 완료 -> 사물함 컴포넌트 표시 */}
+        {selectedBuilding !== null && selectedFloor !== null && (
+          <div
+            className={`absolute inset-y-0 left-0 right-0 border-gray-400 pt-16 flex ${
+              selectedCabinet ? "w-8/12" : "w-full"
+            }`}
+          >
+            <>
+              <div className="absolute inset-y-0 left-12 right-8 pt-16">
+                <CabinetButtonComponent
+                  rows={4}
+                  columns={12}
+                  selectedBuilding={buildings[selectedBuilding]}
+                  selectedFloor={
+                    buildings[selectedBuilding]?.floors[selectedFloor]
+                  }
+                  setSelectedCabinet={setSelectedCabinet}
+                />
+              </div>
+              {/* 화면 크기 = 768px 이하일 때 사물함 정보 숨김 */}
+              <div className="hidden sl:flex">
+                <CabinetStatusInformation />
+              </div>
+            </>
+          </div>
+        )}
+
+        {/* 사물함 선택 완료 -> cabinetRental 컴포넌트 표시 */}
+        {selectedCabinet && (
+          <div className="absolute inset-y-0 right-0 w-80 border-gray-400 border-l-2 pt-20 flex">
+            <SelectedCabinetInformation selectedCabinet={selectedCabinet} />
+          </div>
+        )}
+      </div>
     </div>
   );
 };
