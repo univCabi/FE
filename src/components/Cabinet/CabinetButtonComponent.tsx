13d9d2afabb3d6c5f1edebfb1a244ea938317776
KimJuheee <3098k@naver.com>
2024-11-17 01:53:47 +0900

    Feat: 사물함 상태에 따른 사물함 버튼 색상 변경#26

--- src/components/Cabinet/CabinetButtonComponent.tsx
+++ src/components/Cabinet/CabinetButtonComponent.tsx
@@ -25,7 +25,6 @@ const CabinetButtonComponent = ({
   const handleCabinetCall = async (building: string, floor: number) => {
     try {
       const response = await cabinetCallApi(building, floor);
-      // setCabinetData(response?.cabinets || []); // 응답 데이터에서 사물함 목록 업데이트
       setCabinetData(response.cabinets);
     } catch (error) {
       console.log(error);
@@ -39,15 +38,41 @@ const CabinetButtonComponent = ({
     }
   }, [selectedBuilding, selectedFloor]);
 
+  // 각 상태에 대한 버튼 색상 설정
+  const getStatusColor = (status: string) => {
+    switch (status) {
+      case "MINE":
+        return "bg-lime-500"; // 내 사물함
+      case "USING":
+        return "bg-purple-500"; // 사용 중
+      case "OVERDUE":
+        return "bg-red-500"; // 반납 지연
+      case "AVAILABLE":
+        return "bg-gray-300"; // 이용 가능
+      case "BROKEN":
+        return "bg-gray-700"; // 사용 불가
+    }
+  };
+
+  // 각 상태에 따른 텍스트 색상 설정
+  const getStatusTextColor = (status: string) => {
+    if (status === "AVAILABLE" || status === "MINE") {
+      return "text-black"; // AVAILABLE, MINE일 경우 text-black
+    }
+    return "text-white"; // 나머지 상태는 text-white
+  };
+
   return (
     <div className="w-full">
       <div className="relative top-14 left-1/3 w-[30rem] h-5/6 flex items-center justify-center overflow-scroll">
         {cabinetData.map((cabinet) => (
           <button
-            key={cabinet.cabinetNumber} // 데이터의 cabinetNumber를 키로 사용
-            className="absolute w-16 h-20 rounded-md bg-gray-300 text-gray-500 text-sm hover:bg-gray-200 flex items-end p-2"
+            key={cabinet.cabinetNumber}
+            className={`absolute w-16 h-20 rounded-md hover:bg-opacity-80 flex items-end text-sm p-2 ${getStatusColor(
+              cabinet.status
+            )} ${getStatusTextColor(cabinet.status)}`}
             style={{
-              top: `${1000 - cabinet.yPos}px`, // // API에서 받은 yPos 사용 -> yPos 값을 반전 (yPos 값이 작은 cabinet이 위로 배치됨)
+              top: `${1000 - cabinet.yPos}px`, // API에서 받은 yPos 사용
               left: `${cabinet.xPos}px`, // API에서 받은 xPos 사용
             }}
             onClick={() => setSelectedCabinet(cabinet.cabinetNumber)}
