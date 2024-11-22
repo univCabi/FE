6bde56f95203a3062039a6246f1c2def49eb75ec
민웅기 <d34sda334@gmail.com>
2024-11-22 20:52:50 +0900

    Fix: 로그인 폼 너비및 패딩 수정#35

--- src/components/LoginForm.tsx
+++ src/components/LoginForm.tsx
@@ -27,16 +27,16 @@ const LoginForm = ({
   return (
     <form
       onSubmit={onSubmit}
-      className="sl:w-[60%] w-[80%] sl:max-w-[22.5rem]  flex flex-col items-center p-16 sl:p-10 bg-white rounded-lg shadow-lg"
+      className="sl:w-[60%] w-[70%] sl:max-w-[22.5rem]  flex flex-col items-center p-12 sl:p-10 bg-white rounded-lg shadow-lg"
     >
       <h2 className="text-[2.25em] font-bold mb-5">Login</h2>
-      <p className="text-blue-600 mb-5 text-[0.85rem] sm:text-base">
+      <p className="text-blue-600 mb-5 text-[0.75rem] sm:text-base">
         여러분의 일상을 가볍게
       </p>
       <img className="w-[80%] sl:w-[60%] max-w-[12rem] mb-5" src={loginLogo} />
       <label className="sm:text-xl mb-2">학번</label>
       <input
-        className="text-base w-[120%] sl:w-[90%] px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring focus:ring-blue-500 mb-2"
+        className="text-sm sm:text-base w-[120%] sl:w-[90%] px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring focus:ring-blue-500 mb-2"
         type="text"
         value={studentNumber}
         onChange={(e) => setStudentNumber(e.target.value)}
@@ -44,7 +44,7 @@ const LoginForm = ({
       />
       <label className="sm:text-xl mb-2">비밀번호</label>
       <input
-        className="text-base w-[120%] sl:w-[90%] px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring focus:ring-blue-500 mb-4"
+        className="text-sm sm:text-base w-[120%] sl:w-[90%] px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring focus:ring-blue-500 mb-4"
         type="password"
         value={password}
         onChange={(e) => setPassword(e.target.value)}
@@ -57,7 +57,7 @@ const LoginForm = ({
       )}
       <button
         type="submit"
-        className="w-[60%] sl:w-[70%]  text-white py-2.5 sl:py-3.5 bg-blue-600 rounded-md text-base"
+        className=" text-sm sm:text-base w-[60%] sl:w-[70%]  text-white py-2.5 sl:py-3.5 bg-blue-600 rounded-md "
       >
         L o g i n
       </button>
