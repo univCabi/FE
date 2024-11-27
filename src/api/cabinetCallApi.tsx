a22d7fdb5a88c6cfff8ddf7331ddd8ce82a462d8
KimJuheee <3098k@naver.com>
2024-11-27 20:36:54 +0900

    Fix: cabinetCallApi.tsx error 해결#37

--- src/api/cabinetCallApi.tsx
+++ src/api/cabinetCallApi.tsx
@@ -3,7 +3,6 @@ import api from "@/api/axiosInterceptApi";
 // 메인 페이지: 건물, 층 선택 시 호출되는 API
 export const cabinetCallApi = async (building: string, floor: number) => {
   try {
-<<<<<<<<< Temporary merge branch 1
     const response = await api.get(
       `/cabinet?building=${building}&floor=${floor}`
     );
