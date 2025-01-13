a8f9f21496b43981466d82b8373b9fd25780d083
KimJuheee <3098k@naver.com>
2025-01-14 01:09:35 +0900

    Refactor: 사물함 color 함수 분리#50

--- src/hooks/useCabinetState.tsx
+++ src/hooks/useCabinetState.tsx
@@ -47,6 +47,26 @@ export const useCabinetState = () => {
     }
   };
 
+  // 각 상태에 대한 버튼 색상 설정
+  const getStatusColor = (selectedStatus: string, isMyCabinet: boolean) => {
+    if (selectedStatus === "USING") {
+      if (isMyCabinet === true) {
+        return "bg-lime-500 text-white"; // 본인이 사용 중인 사물함
+      }
+      if (isMyCabinet === false) {
+        return "bg-purple-500 text-white"; // 다른 사람이 사용 중인 사물함
+      }
+    }
+    switch (selectedStatus) {
+      case "OVERDUE":
+        return "bg-red-500 text-white"; // 반납 지연
+      case "AVAILABLE":
+        return "bg-gray-300"; // 이용 가능
+      case "BROKEN":
+        return "bg-gray-700 text-white"; // 사용 불가
+    }
+  };
+
   useEffect(() => {
     if (selectedCabinet === null) return;
     if (selectedCabinet.cabinetId) {
@@ -65,6 +85,7 @@ export const useCabinetState = () => {
     isMyCabinet,
     setIsMyCabinet,
     fetchCabinetDetailInformation,
+    getStatusColor,
     getStatusColor,
   };
 };
