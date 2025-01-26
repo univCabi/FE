ed20a0ea4f5a8e30262f4295e1904ad64ef5965f
minwoonggi <d34sda334@gmail.com>
2025-01-26 18:38:37 +0900

    Feat: HistoryList 무한 스크롤 구현#58

--- src/api/userHistoryDataApi.tsx
+++ src/api/userHistoryDataApi.tsx
@@ -1,10 +1,13 @@
 import api from "@/api/axiosInterceptApi";
 
-export const userHistoryDataApi = async () => {
+export const userHistoryDataApi = async (page: number, pageSize: number) => {
   try {
-    const response = await api.get("/cabinet/history");
+    const response = await api.get("/cabinet/history", {
+      params: { page, pageSize },
+    });
     return { status: response.status, data: response.data };
   } catch (error) {
+    console.log("HistoryDataApi 호출 중 오류", error);
     throw error; // 오류를 발생시켜 useUserData의 catch 블록으로 전달
   }
 };
--- src/hooks/useHistoryData.tsx
+++ src/hooks/useHistoryData.tsx
@@ -1,5 +1,6 @@
+import { useEffect, useState, useCallback } from "react";
+import { throttle } from "lodash";
 import { userHistoryDataApi } from "@/api/userHistoryDataApi";
-import { useEffect, useState } from "react";
 interface HistoryData {
   building: string;
   floor: number;
@@ -11,18 +12,45 @@ interface HistoryData {
 
 export const useHistoryData = () => {
   const [userHistoryData, setUserHistoryData] = useState<HistoryData[]>([]);
+  const [page, setPage] = useState<number>(1);
+  const [hasMoreResults, setHasMoreResults] = useState<boolean>(true);
+  const [lastElement, setLastElement] = useState<HTMLTableRowElement | null>(
+    null
+  );
+  const pageSize: number = 10;
   useEffect(() => {
-    const getHistoryData = async () => {
+    const fetchHistoryData = async () => {
       try {
-        const response = await userHistoryDataApi();
-        setUserHistoryData(response.data.results);
-        console.log(response.status);
+        const response = await userHistoryDataApi(page, pageSize);
+        if (response.data.results.length === 0) {
+          setHasMoreResults(false);
+        } else {
+          setUserHistoryData((prev) => [...prev, ...response.data.results]);
+        }
       } catch (error) {
-        console.error("로그인 중 오류가 발생했습니다:", error);
         console.log(error.response?.status || "오류를 알 수 없습니다.");
       }
     };
-    getHistoryData();
+    if (hasMoreResults) fetchHistoryData();
+  }, [page]);
+
+  useEffect(() => {
+    if (!lastElement) return;
+    const observer = new IntersectionObserver(
+      throttle((entries) => {
+        if (entries[0].isIntersecting && hasMoreResults) {
+          setPage((prev) => prev + 1);
+        }
+      }, 800), // 스크롤 내린지 0.8초 뒤에 api 호출되도록 설정
+      { threshold: 0.8 }
+    );
+    observer.observe(lastElement);
+    return () => observer.disconnect();
+  }, [lastElement, hasMoreResults]);
+
+  const setObserverRef = useCallback((node: HTMLTableRowElement) => {
+    setLastElement(node); // 해당 <tr> 요소를 관찰 대상으로 지정정
   }, []);
-  return { userHistoryData };
+
+  return { userHistoryData, setObserverRef };
 };
