8d22e6433a1bad45035dc5c397d8249d71500bc5
김주희 <kimjuhee@gimjuhuis-MacBook-Air.local>
2025-02-19 03:18:41 +0900

    Fix: 드롭다운 건물 선택시  다른 building 선택 안되는 오류 해결#71

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -39,7 +39,7 @@ const MainPage = () => {
       setSelectedFloor(null);
       setSelectedCabinet(null);
     }
-  }, [location.state, selectedBuilding]);
+  }, [location.state]);
 
   return (
     <>
