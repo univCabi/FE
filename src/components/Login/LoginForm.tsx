c789a685112bc2d5c2d70d3bb5ee5d3506a1d2f7
minwoonggi <d34sda334@gmail.com>
2025-03-17 19:02:31 +0900

    refactor: SVG 컴포넌트 리렌더링 최적화#81

--- src/components/Login/LoginForm.tsx
+++ src/components/Login/LoginForm.tsx
@@ -1,3 +1,4 @@
+import { memo } from "react";
 import SubmitAndNavigateButton from "@/components/SubmitAndNavigateButton";
 import { useHandleLogin } from "@/hooks/useHandleLogin";
 import LogoSVG from "@/icons/cabiLogo.svg?react";
@@ -22,12 +23,13 @@ const LoginForm = ({ idInputLabel }: LoginFormProps) => {
     setPassword("");
     setStudentNumber("");
   };
+  const MemoizedLogoSVG = memo(LogoSVG);
 
   return (
     <>
       {loading && (
         <div className="fixed inset-0 bg-gray-600 bg-opacity-50 flex justify-center items-center z-50">
-          <LogoSVG className="w-[80%] h-[15%] sl:w-[60%] animate-spin  mb-5" />
+          <MemoizedLogoSVG className="w-[80%] h-[15%] sl:w-[60%] animate-spin  mb-5" />
         </div>
       )}
       <form className="sl:w-[60%] w-[70%] sl:max-w-[22.5rem]  flex flex-col items-center p-12 sl:p-10 bg-white rounded-lg shadow-lg">
@@ -35,7 +37,7 @@ const LoginForm = ({ idInputLabel }: LoginFormProps) => {
         <p className="text-blue-600 mb-5 text-[0.75rem] sm:text-base">
           여러분의 일상을 가볍게
         </p>
-        <LogoSVG className="w-[80%] h-[50%] sl:w-[60%]  mb-5" />
+        <MemoizedLogoSVG className="w-[80%] h-[50%] sl:w-[60%]  mb-5" />
         <label className="sm:text-xl mb-2">{idInputLabel}</label>
         <input
           className="text-sm sm:text-base w-[120%] sl:w-[90%] px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring focus:ring-blue-500 mb-2"
