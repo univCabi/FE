eb4a4004a6b073f018c1b9ce99b3b58b5472e754
김주희 <kimjuhee@gimjuhuis-MacBook-Air.local>
2025-03-21 15:53:35 +0900

    Feat: formatDate 함수 utils 함수로 분리#83

--- src/components/Admin/Cabinet/AdminCabinetInformationDisplay.tsx
+++ src/components/Admin/Cabinet/AdminCabinetInformationDisplay.tsx
@@ -1,4 +1,5 @@
 import { CabinetInfo, SelectedMultiCabinetsData } from "@/types/CabinetType";
+import { formatDate } from "@/utils/formatDate";
 import CabinetActionButtons from "@/components/Cabinet/CabinetActionButtons";
 import CabinetSVG from "@/icons/cabinet.svg?react";
 
@@ -12,17 +13,6 @@ interface AdminCabinetInformationDisplayProps extends CabinetInfo {
   selectedStatus: string;
   expiredAt: string | null;
 }
-
-const formatDate = (isoString: string | null): string => {
-  if (!isoString) return "날짜 정보 없음";
-  const date = new Date(isoString);
-  const options: Intl.DateTimeFormatOptions = {
-    year: "numeric",
-    month: "2-digit",
-    day: "2-digit",
-  };
-  return date.toLocaleDateString("ko-KR", options);
-};
 
 const AdminCabinetInformationDisplay = ({
   selectedBuilding,
---
+++ src/utils/formatDate.ts
@@ -0,0 +1,11 @@
+// 날짜 포맷팅 함수
+export const formatDate = (isoString: string | null): string => {
+  if (!isoString) return "날짜 정보 없음";
+  const date = new Date(isoString);
+  const options: Intl.DateTimeFormatOptions = {
+    year: "numeric",
+    month: "2-digit",
+    day: "2-digit",
+  };
+  return date.toLocaleDateString("ko-KR", options);
+};
